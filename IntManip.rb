59e5ccfdc5a0e716ad7b5746782a27723bf73f44
Hellpi Mappa <baudetad@gmail.com>
2015-09-27 18:22:36 +0200

    Update IntManip.rb

--- IntManip.rb
+++ IntManip.rb
@@ -6,3 +6,28 @@ def decr(array)
 	element = int(calc(look_at(array, 0, 0)))
 	(element - 1)
 end
+def somme(array)
+	element = int(calc(look_at(array, 0, 0)))
+	ajout = int(calc(look_at(array, 1, 0)))
+	element + ajout
+end
+def difference(array)
+	element = int(calc(look_at(array, 0, 0)))
+	ajout = int(calc(look_at(array, 1, 0)))
+	element - ajout
+end
+def produit(array)
+	element = int(calc(look_at(array, 0, 0)))
+	ajout = int(calc(look_at(array, 1, 0)))
+	element * ajout
+end
+def division(array)
+	element = int(calc(look_at(array, 0, 0)))
+	ajout = int(calc(look_at(array, 1, 0)))
+	element / ajout
+end
+def puissance(array)
+	element = int(calc(look_at(array, 0, 0)))
+	ajout = int(calc(look_at(array, 1, 0)))
+	element**ajout
+end
