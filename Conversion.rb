eccb4f42d6d3492d29133d769ee59143c256fa87
Hellpi Mappa <baudetad@gmail.com>
2015-12-30 12:00:02 +0100

    Update Conversion.rb

--- Conversion.rb
+++ Conversion.rb
@@ -1,9 +1,5 @@
 def str(arg)
-	begin
-		argbis = arg.to_s
-	rescue
-		argbis = ""
-	end
+	argbis = arg.to_s
 	if argbis[0] == "'"
 		argbis[1..-2]
 	else
@@ -26,6 +22,25 @@ def arr(arg)
 	end
 	argbis.map!{|i| calc(i)}
 	argbis
+end
+def arrbis(arg)
+	if arg[0] == "["
+		arg = arg[1..-2].gsub("[", "").gsub("]", "").split(",")
+		arg.each{|i| i.strip!}
+	end
+	begin
+		argbis = arg.to_a
+	rescue
+		argbis = [arg]
+	end
+	argbis
+end
+def bool(arg)
+	if calc(arg)
+		true
+	else
+		false
+	end
 end
 def lamb(args)
 	
