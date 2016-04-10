005497622c3f705b0b7eca924f0bf026ed881431
Hellpi Mappa <baudetad@gmail.com>
2016-04-10 22:15:56 +0200

    Mise a jour

--- Version 1/Conversion.rb
+++ Version 1/Conversion.rb
@@ -10,6 +10,9 @@ def int(arg)
 	arg = str(arg)
 	arg.to_i
 end
+def intbis(array)
+	int(calc(look_at(array, 0, 0)))
+end
 def arr(arg)
 =begin
 	if arg[0] == "["
@@ -22,14 +25,15 @@ def arr(arg)
 	rescue
 		argbis = [arg]
 	end
-	argbis.map!{|i| calc(i)}
-	argbis
+	argbis.map{|i| calc(i)}
 end
 def arrbis(arg)
+=begin
 	if arg[0] == "["
 		arg = arg[1..-2].gsub("[", "").gsub("]", "").split(",")
 		arg.each{|i| i.strip!}
 	end
+=end
 	begin
 		argbis = arg.to_a
 	rescue
@@ -42,6 +46,17 @@ def bool(arg)
 		true
 	else
 		false
+	end
+end
+def dynconv(arg, argbis)
+	if arg.is_a?(Integer)
+		int(argbis)
+	elsif arg.is_a?(Array)
+		arr(argbis)
+	elsif arg.is_a?(String)
+		str(argbis)
+	else
+		bool(argbis)
 	end
 end
 def lamb(args)
