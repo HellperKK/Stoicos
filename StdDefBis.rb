5cd5693b8a5826a1c097eb9c06bb875f4850567f
François Berhn <baudetad@gmail.com>
2018-02-10 13:38:20 +0100

    Update

--- StdDefBis.rb
+++ StdDefBis.rb
@@ -1,17 +1,30 @@
-stringMod = Hash.new(Value.new("unit", nil))
+##Modif de string
+stringMod = Hash.new($vars.unit)
 
 stringMod["get"] = NativeFunction.new("fun", lambda do |array|
-  first = look_at(array, 0).total_manip("int").value
-  second = look_at(array, 1).total_manip("string").value
-  Value.new("string", second[first])
+  first = look_at(array, 0).total_manip("string").value
+  second = look_at(array, 1).total_manip("int").value
+  Value.new("string", first[second])
 end)
 
-stringMod["set"] = NativeFunction.new("fun", lambda do |array|
-  first = look_at(array, 0).total_manip("int").value
-  second = look_at(array, 1).total_manip("string").value
-  third = look_at(array, 2).total_manip("string").value
-  second[first] = third
-  Value.new("string", second)
+stringMod["slice"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("string").value
+  second = look_at(array, 1).total_manip("int").value
+  third = look_at(array, 2).total_manip("int").value
+  Value.new("string", first[second...third])
+end)
+
+stringMod["sub"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("string").value
+  second = look_at(array, 1).total_manip("int").value
+  third = look_at(array, 2).total_manip("int").value
+  maximum = second + third
+  Value.new("string", first[second...maximum])
+end)
+
+stringMod["strip"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("string").value
+  Value.new("string", first.strip)
 end)
 
 $vars.set_value("String", Value.new("struct", stringMod))
