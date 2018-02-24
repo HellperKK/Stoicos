784d8dd157f18a4ac6df194764993804f0cefc23
François Berhn <baudetad@gmail.com>
2018-02-24 10:26:21 +0100

    Update gameDef

--- GameDef.rb
+++ GameDef.rb
@@ -6,9 +6,7 @@ class GameWindow < Gosu::Window
 		@font = Gosu::Font.new(18)
 		# @input = InputManager.new([Gosu::KbUp, Gosu::KbDown, Gosu::KbLeft, Gosu::KbRight, Gosu::KbSpace, Gosu::MsLeft], lambda{|i| button_down?(i)})
     @sprites = []
-	end
-	def button_down(id)
-		@input.addPress(id)
+    @showed = false
 	end
 	def update
 		@input = []
@@ -16,11 +14,43 @@ class GameWindow < Gosu::Window
 	def draw
     @sprites.each{|i| i.draw}
 	end
+  def show
+    @showed = true
+    super
+  end
+
+  def button_down(id)
+		@input << id
+	end
+  def pressed?(id)
+    @input.include?(id)
+  end
+  def pressing?(id)
+    button_down?(id)
+  end
+
+  def showed?
+    @showed
+  end
 end
+
+$game = GameWindow.new
 
 ##Modif de jeu
 gameMod = Hash.new($vars.unit)
 
-gameMod["get"] = NativeFunction.new("fun", lambda do |array|
-  $vars.set_value("String", Value.new("struct", stringMod))
+gameMod["key"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("string").value
+  case first
+  when "UP" then Value.new("int", Gosu::KbUp)
+  when "DOWN" then Value.new("int", Gosu::KbDown)
+  when "LEFT" then Value.new("int", Gosu::KbLeft)
+  when "RIGHT" then Value.new("int", Gosu::KbRight)
+  when "SPACE" then Value.new("int", Gosu::KbSpace)
+  else Value.new("int", Gosu::KbSpace)
+  end
 end)
+
+$vars.set_value("Game", Value.new("struct", gameMod))
+
+puts Gosu::KbUp
