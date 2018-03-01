537fdff54924a2bfdea722ff3832601533cc107b
Adrien Baudet <baudetad@gmail.com>
2018-03-01 11:32:36 +0100

    Start file module

--- Src/FileManager.rb
+++ Src/FileManager.rb
@@ -3,13 +3,27 @@ class FileManager
     @path = path
     @files = [file]
   end
-  def load(file)
+  def import(file)
     unless @files.include?(file)
-      execute_file("#{@path}/#{file}")
-      @files << file
+      begin
+        execute_file("#{@path}/#{file}")
+        @files << file
+      rescue
+        
+      end
     end
   end
   def main
     "#{@path}/#{@files[0]}"
   end
 end
+
+fileMod = Hash.new($vars.unit)
+
+fileMod["import"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("string").value
+  $chemin.import(first)
+  $vars.unit
+end)
+
+$vars.set_value("File", Value.new("struct", fileMod))
