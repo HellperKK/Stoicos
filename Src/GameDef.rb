8b5697b16079dbe765eb627949174d879bba885f
François Berhn <baudetad@gmail.com>
2018-03-05 13:03:57 +0100

    Update gameDef

--- Src/GameDef.rb
+++ Src/GameDef.rb
@@ -1,9 +1,20 @@
 require "gosu"
+class GameSprite < Gosu::Image
+	def initialize(name, x, y)
+		super(name)
+		@x = x
+		@y = y
+	end
+	def draw
+		super(@x, @y, 0)
+	end
+end
 class GameWindow < Gosu::Window
 	def initialize
 		super(640, 480, false)
 		self.caption = "JeuCartesVerehn"
 		@font = Gosu::Font.new(18)
+		@input = []
 		# @input = InputManager.new([Gosu::KbUp, Gosu::KbDown, Gosu::KbLeft, Gosu::KbRight, Gosu::KbSpace, Gosu::MsLeft], lambda{|i| button_down?(i)})
     @sprites = []
     @showed = false
@@ -12,15 +23,22 @@ class GameWindow < Gosu::Window
 		if @input.include?(Gosu::KbEscape)
 			self.close!
 		end
+		@value = @update.call([@value])
+		@draw.call([@value])
 		@input = []
 	end
 	def draw
     @sprites.each{|i| i.draw}
 	end
-  def show
-    @showed = true
-    super
-  end
+  def start(init, update, draw)
+		@showed = true
+		@value = init.call([$vars.unit])
+		puts [@value].to_s
+		puts [init].to_s
+		@update = update
+		@draw = draw
+		self.show
+	end
 
   def button_down(id)
 		@input << id
@@ -35,6 +53,17 @@ class GameWindow < Gosu::Window
   def showed?
     @showed
   end
+
+	def clean_screen
+		@sprites = []
+	end
+	def add_sprite(name, x, y)
+		begin
+			@sprites << GameSprite.new("#{$chemin.path}/#{name}", x, y)
+		rescue
+
+		end
+	end
 end
 
 $game = GameWindow.new
@@ -54,14 +83,54 @@ gameMod["key"] = NativeFunction.new("fun", lambda do |array|
   end
 end)
 
+gameMod["start"] = NativeFunction.new("fun", lambda do |array|
+	unless $game.showed?
+		first = look_at(array, 0).total_manip("fun")
+		second = look_at(array, 1).total_manip("fun")
+		thrid = look_at(array, 2).total_manip("fun")
+	  $game.start(first, second, thrid)
+	else
+		$vars.unit
+	end
+end)
+
 gameMod["pressed"] = NativeFunction.new("fun", lambda do |array|
-  first = look_at(array, 0).total_manip("int").value
-  Value.new("bool", $game.pressed?(first))
+	if $game.showed?
+	  first = look_at(array, 0).total_manip("int").value
+	  Value.new("bool", $game.pressed?(first))
+	else
+		$vars.unit
+	end
 end)
 
 gameMod["pressing"] = NativeFunction.new("fun", lambda do |array|
-  first = look_at(array, 0).total_manip("int").value
-  Value.new("bool", $game.pressing?(first))
+	if $game.showed?
+		first = look_at(array, 0).total_manip("int").value
+	  Value.new("bool", $game.pressing?(first))
+	else
+		$vars.unit
+	end
+end)
+
+gameMod["clean_screen"] = NativeFunction.new("fun", lambda do |array|
+	if $game.showed?
+		$game.clean_screen
+		$vars.unit
+	else
+		$vars.unit
+	end
+end)
+
+gameMod["draw"] = NativeFunction.new("fun", lambda do |array|
+	if $game.showed?
+		first = look_at(array, 0).total_manip("string").value
+		second = look_at(array, 1).total_manip("int").value
+		third = look_at(array, 2).total_manip("int").value
+		$game.add_sprite(first, second, third)
+		$vars.unit
+	else
+		$vars.unit
+	end
 end)
 
 $vars.set_value("Game", Value.new("struct", gameMod))
