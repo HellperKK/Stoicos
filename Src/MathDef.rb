13bff025d069181c285679e1e0c608050285821b
François Berhn <baudetad@gmail.com>
2018-03-29 17:36:38 +0200

    Changed math.random

--- Src/MathDef.rb
+++ Src/MathDef.rb
@@ -9,4 +9,16 @@ mathMod["sqrt"] = NativeFunction.new("fun", lambda do |array|
   Value.new("float", Math.sqrt(first))
 end)
 
+mathMod["randint"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("int").value
+  second = look_at(array, 1).total_manip("int").value
+  Value.new("int", rand(first..second))
+end)
+
+mathMod["randfloat"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("float").value
+  second = look_at(array, 1).total_manip("float").value
+  Value.new("float", rand(first..second))
+end)
+
 $vars.set_value("Math", Value.new("struct", mathMod))
