cd4442d574758ea0d55ac854c8e2428323070de0
François Berhn <baudetad@gmail.com>
2018-04-16 08:38:00 +0200

    added struct.extend

--- Src/StructDef.rb
+++ Src/StructDef.rb
@@ -11,4 +11,13 @@ structMod["update"] = NativeFunction.new("fun", lambda do |array|
 	Value.new("struct", first)
 end)
 
+structMod["extend"] = NativeFunction.new("fun", lambda do |array|
+	first = look_at(array, 0).total_manip("struct").value
+	second = look_at(array, 1).total_manip("symbol").value
+	third = look_at(array, 2).get.calculate
+	first =  first.clone
+	first[second] = third
+	Value.new("struct", first)
+end)
+
 $vars.set_value("Struct", Value.new("struct", structMod))
