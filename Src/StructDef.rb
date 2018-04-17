18628401a2734089afa96b1a08aa876cc23ab99f
François Berhn <baudetad@gmail.com>
2018-04-17 10:10:33 +0200

    Added struct.update_fun

--- Src/StructDef.rb
+++ Src/StructDef.rb
@@ -11,6 +11,17 @@ structMod["update"] = NativeFunction.new("fun", lambda do |array|
 	Value.new("struct", first)
 end)
 
+structMod["update_fun"] = NativeFunction.new("fun", lambda do |array|
+	first = look_at(array, 0).total_manip("struct").value
+	second = look_at(array, 1).total_manip("symbol").value
+	third = look_at(array, 2).total_manip("fun")
+	if first.include?(second)
+		first =  first.clone
+		first[second] = third.call([first[second]])
+	end
+	Value.new("struct", first)
+end)
+
 structMod["extend"] = NativeFunction.new("fun", lambda do |array|
 	first = look_at(array, 0).total_manip("struct").value
 	second = look_at(array, 1).total_manip("symbol").value
