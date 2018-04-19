e3dfe469a7b6f97158e4204e845bcfc575493f2a
François Berhn <baudetad@gmail.com>
2018-04-19 19:36:30 +0200

    Update

--- Src/FileManager.rb
+++ Src/FileManager.rb
@@ -17,6 +17,9 @@ class FileManager
   def main
     "#{@path}/#{@files[0]}"
   end
+  def relative(file)
+    "#{@path}/#{file}"
+  end
 end
 
 fileMod = Hash.new($vars.unit)
@@ -30,7 +33,7 @@ end)
 fileMod["read"] = NativeFunction.new("fun", lambda do |array|
   first = look_at(array, 0).total_manip("string").value
   begin
-    Value.new("string", File.open("#{$chemin.path}/#{first}", "r"){|i| i.read})
+    Value.new("string", File.open($chemin.relative(first), "r"){|i| i.read})
   rescue
     $vars.unit
   end
@@ -39,7 +42,7 @@ end)
 fileMod["write"] = NativeFunction.new("fun", lambda do |array|
   first = look_at(array, 0).total_manip("string").value
   second = look_at(array, 1).total_manip("string").value
-  File.open("#{$chemin.path}/#{first}", "w"){|i| i.write(second)}
+  File.open($chemin.relative(first), "w"){|i| i.write(second)}
   $vars.unit
 end)
 
