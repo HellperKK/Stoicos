611656165ab8c415cf6774a932aaf6baaf22bab4
François Berhn <baudetad@gmail.com>
2018-05-30 10:40:50 +0200

    Added math functions

--- Src/MathDef.rb
+++ Src/MathDef.rb
@@ -21,4 +21,34 @@ mathMod["randfloat"] = NativeFunction.new("fun", lambda do |array|
   Value.new("float", rand(first..second))
 end)
 
+mathMod["acos"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("float").value
+  Value.new("float", Math.acos(first))
+end)
+
+mathMod["cos"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("float").value
+  Value.new("float", Math.cos(first))
+end)
+
+mathMod["atan"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("float").value
+  Value.new("float", Math.atan(first))
+end)
+
+mathMod["tan"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("float").value
+  Value.new("float", Math.tan(first))
+end)
+
+mathMod["asin"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("float").value
+  Value.new("float", Math.asin(first))
+end)
+
+mathMod["sin"] = NativeFunction.new("fun", lambda do |array|
+  first = look_at(array, 0).total_manip("float").value
+  Value.new("float", Math.sin(first))
+end)
+
 $vars.set_value("Math", Value.new("struct", mathMod))
