c20e7affd66f74d824c4983625333cc5e6307683
Adrien Baudet <baudetad@gmail.com>
2019-11-25 17:25:48 +0100

    tiny tweak

--- src/values/FunCall.rb
+++ src/values/FunCall.rb
@@ -2,7 +2,7 @@ class FunCall < Value
   def calc
     if @value.length > 0
       fun = @value[0].get.calc
-      args = @value[1..-1]
+      args = @value[1..-1].map{|el| el.get.calc}
       args ||= []
       fun.call(args)
     else
