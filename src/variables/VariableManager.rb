bdd88229881f245af11f96c14a8a689bf6d890ed
Adrien Baudet <baudetad@gmail.com>
2019-11-25 13:55:49 +0100

    cleaned variables

--- src/variables/VariableManager.rb
+++ src/variables/VariableManager.rb
@@ -67,17 +67,3 @@ class VariableManager
 		@vars[-1].to_s
 	end
 end
-
-class Variable
-	attr_reader :value
-	def initialize(value, cons)
-		@value = value
-		@cons = cons
-		@type = "dynamic"
-	end
-	def set_value(value)
-		raise "Illegal constant set" if @cons
-		value = value.convert(@type) unless @type == "dynamic"
-		@value = value
-	end
-end
